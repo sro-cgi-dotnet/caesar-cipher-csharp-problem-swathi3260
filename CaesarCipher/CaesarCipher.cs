fbb53d7010498f69aef23a663702dd45b2e04f15
Nishant Jain <nishantkumarajain@gmail.com>
2018-07-17 15:24:06 +0530

    Initial Commit

---
+++ CaesarCipher/CaesarCipher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace CaesarCipher
+{
+    public static class RotationalCipher
+    {
+        public static string Rotate(string text, int shiftKey)
+        {
+            throw new NotImplementedException("You need to implement this function.");
+        }
+    }
+}
